04a7396149f5d562ddb558d6dbd6003c6b58e5e4
sadjabeng003 <sadjabeng003@st.ug.edu.gh>
2024-07-06 11:10:23 +0000

    Add ticket price calculator

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int regularPrice = 10;
+            const int discountedPrice = 7;
+
+            Console.WriteLine("Enter your age:");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age <= 12 || age >= 65)
+            {
+                Console.WriteLine($"Ticket Price: GHC{discountedPrice}");
+            }
+            else
+            {
+                Console.WriteLine($"Ticket Price: GHC{regularPrice}");
+            }
+        }
+    }
+}
+
