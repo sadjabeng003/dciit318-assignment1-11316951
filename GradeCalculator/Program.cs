90bc0b9f1b933081f47cf3b6a7c5c04d4a14a9f2
sadjabeng003 <sadjabeng003@st.ug.edu.gh>
2024-07-06 11:05:53 +0000

    Add grade calculator

---
+++ GradeCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GradeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a numerical grade between 0 and 100:");
+            int grade = int.Parse(Console.ReadLine());
+
+            if (grade >= 90)
+            {
+                Console.WriteLine("Grade: A");
+            }
+            else if (grade >= 80)
+            {
+                Console.WriteLine("Grade: B");
+            }
+            else if (grade >= 70)
+            {
+                Console.WriteLine("Grade: C");
+            }
+            else if (grade >= 60)
+            {
+                Console.WriteLine("Grade: D");
+            }
+            else
+            {
+                Console.WriteLine("Grade: F");
+            }
+        }
+    }
+}
